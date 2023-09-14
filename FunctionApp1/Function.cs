c073768d59ad035e74e6f2e3ff4fb5d59f676200
marcop <marcop@microsoft.com>
2023-09-14 13:05:35 -0700

    function basic trigger functionality

---
+++ FunctionApp1/Function.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Data;
+using System.Text.Json.Nodes;
+
+namespace FunctionApp1
+{
+
+
+    public class Function1
+    {
+        private readonly string CV_Endpoint;
+        private readonly string CV_Key;
+        private readonly string Blob_ConnectionString;
+        private readonly string Library_Name;
+
+        private UVS_V1_Wrapper uvs;
+
+
+        public Function1(IConfiguration config)
+        {
+            CV_Endpoint = config["computer_vision_endpoint"];
+            CV_Key = config["computer_vision_key"];
+            Blob_ConnectionString = config["libraryConnectionString"];
+            Library_Name = config["library_name"];
+            uvs = new UVS_V1_Wrapper(CV_Endpoint, CV_Key, Library_Name);
+        }
+
+        private (string,string) parseBlobUrl(string fileBlobUrl)
+        {
+            Uri blobUri = new Uri(fileBlobUrl);
+            string containerName = blobUri.Segments[1].TrimEnd('/');
+            string fileName = blobUri.Segments[2];
+
+            return(containerName, fileName);
+        }
+
+
+        private string GenerateDocumentId(string fileBlobUrl)
+        {
+            (string, string) parsedUrl = parseBlobUrl(fileBlobUrl);
+            return parsedUrl.Item2;
+        }
+
+        private string GenerateSaSUrl(string fileBlobUrl)
+        {
+             (string,string) parsedUrl = parseBlobUrl(fileBlobUrl);
+            // Create a BlobClient object from the blob URL and connection string
+            BlobClient blobClient = new BlobClient(Blob_ConnectionString, parsedUrl.Item1, parsedUrl.Item2);
+
+            // Create a BlobSasBuilder object to specify the SAS parameters
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                // Set the expiry time and permissions for the SAS
+                ExpiresOn = DateTime.UtcNow.AddHours(1),
+                BlobContainerName = blobClient.BlobContainerName,
+                BlobName = blobClient.Name,
+                Resource = "b"
+            };
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+            Uri sasUri = blobClient.GenerateSasUri(sasBuilder);
+
+            return sasUri.ToString();
+        }
+
+        private async Task TriggerIndex(string fileBlobUrl)
+        {
+            return;
+        }
+
+        [FunctionName("BlobTrigger")]
+        public async void Run([BlobTrigger("videolibrary/{name}", Connection = "libraryConnectionString")] BlobClient myQueueItem, ILogger log)
+        {
+            
+            log.LogInformation($"C# Queue trigger function processed; {myQueueItem.Uri.ToString()}");
+
+            string SasUrl = GenerateSaSUrl(myQueueItem.Uri.ToString());
+
+            var docId = GenerateDocumentId(myQueueItem.Uri.ToString());
+
+            var result = await uvs.addDocumentToIndex(SasUrl, docId);
+            
+            log.LogInformation($"C# sas url is {SasUrl}");
+        }
+    }
+}
---
+++ FunctionApp1/UVS.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+public class UVS_V2_Wrapper
+{
+    private HttpClient _httpClient;
+    private readonly string cv_Endpoint;
+    private readonly string cv_Key;
+    private readonly string library_Name;
+
+    private readonly string api_version = "2023-05-01-preview";
+
+    internal static class FeatureModelName
+    {
+        static string VISION = "vision";
+        static string SPEECH = "speech";
+    }
+
+    internal static class FeatuerModelDomain
+    {
+        static string GENERIC = "generic";
+        static string MEDIAL = "medial";
+        static string SURVEILLANCE = "surveillance";
+    }
+
+
+    internal class IndexRequestModel
+    {
+    }
+
+    private IndexRequestModel createIngestionIndexRequestBody()
+    {
+        return null;
+    }
+
+    public UVS_V2_Wrapper(string cv_endpoint, string cv_key, string library)
+    {
+        _httpClient = new HttpClient();
+        setDefaultHeaders();
+        cv_Endpoint = cv_endpoint;
+        cv_Key = cv_key;
+        library_Name = library;
+    }
+
+    private readonly string retreival_path = "computervision/retrieval/indexes/";
+
+    private void setDefaultHeaders()
+    {
+        _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", cv_Key);
+    }
+
+    
+
+
+    private async Task<bool> getIndex(string indexName)
+    {
+        var path = retreival_path + indexName;
+        var url = string.Format("{0}{1}?{2}", cv_Endpoint, path, api_version);
+
+        
+        var response = await _httpClient.GetAsync(url);
+
+        if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK) { return true; };
+        return false;
+    }
+
+    private async Task<bool> createIndex(string indexName)
+    {
+        var path = retreival_path + indexName;
+        var url = string.Format("{0}{1}?{2}", cv_Endpoint, path, api_version);
+
+        var body = createIngestionIndexRequestBody();
+
+        var response = await _httpClient.PutAsync(url,null);
+
+        if (response != null && response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            return true;    
+        }
+        return false;
+    }
+
+    private async Task<bool> generateIndexIfNotExists(string indexName)
+    {
+        if (await getIndex(indexName))
+            return false;
+
+        return await createIndex(indexName);
+       
+    }
+
+    private async Task<string> generateIngestion(string indexName, string documentUrl)
+    {
+        return "";
+    }
+
+
+    public void addDocumentToIndex(string documentSaSUrl)
+    {
+        var indexExists = generateIndexIfNotExists(library_Name);
+        var ingestion = generateIngestion(library_Name, documentSaSUrl);
+
+
+    }
+}
+
+
+
+public class UVS_V1_Wrapper
+{
+    private HttpClient _httpClient;
+    private readonly string cv_Endpoint;
+    private readonly string cv_Key;
+    private readonly string library_Name;
+    private readonly string api_version = "2023-01-15-preview";
+
+    public UVS_V1_Wrapper(string cv_endpoint, string cv_key, string library)
+    {
+        _httpClient = new HttpClient();
+        cv_Endpoint = cv_endpoint;
+        cv_Key = cv_key;
+        library_Name = library;
+    }
+
+    private readonly string retreival_path = "vision-retrieval/retrieval/indexes/";
+
+
+
+    public async Task<HttpResponseMessage> addDocumentToIndex(string SaSUrl, string documentId)
+    {
+        var path = retreival_path + library_Name;
+        var url = string.Format("{0}{1}/documents/{2}?{3}", cv_Endpoint, path, documentId, api_version);
+
+        _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", cv_Key);
+        //_httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+        JsonObject ingestionDocument = new JsonObject();
+        ingestionDocument["documentUrl"] = SaSUrl;
+
+        HttpContent content = new StringContent(ingestionDocument.ToJsonString());
+
+        return await _httpClient.PutAsync(url, content);
+
+    }
+}
